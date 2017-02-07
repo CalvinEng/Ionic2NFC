7bcef13feed975bc14dfae1b6bcdcd90f9316c1f
Your Name <you@example.com>
2017-02-07 14:32:44 +0800

    Second Test
    Fixed to work with Ionic2

--- www/Ionic2NFC.js
+++ www/Ionic2NFC.js
@@ -1,5 +1,16 @@
-var exec = require('cordova/exec');
+function Nfc() {}
 
-exports.coolMethod = function(arg0, success, error) {
-    exec(success, error, "Ionic2NFC", "coolMethod", [arg0]);
+Nfc.prototype.coolMethod = function (options, successCallback, errorCallback) {
+    cordova.exec(successCallback, errorCallback, "Nfc", "coolMethod", []);
 };
+
+Nfc.install = function () {
+    if (!window.plugins) {
+        window.plugins = {};
+    }
+
+    window.plugins.Nfc = new Nfc();
+    return window.plugins.Nfc;
+};
+
+cordova.addConstructor(Nfc.install);
